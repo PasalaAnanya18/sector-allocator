75df4d4e9317aac84efca789bde9f060fec9c023
Mukul Anissh G <31366239+mukul-anissh@users.noreply.github.com>
2025-06-04 15:27:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -35,7 +35,9 @@ if 'headlines' in st.session_state:
             model.load_state_dict(AutoModelForSequenceClassification.from_pretrained('yiyanghkust/finbert-tone').state_dict(), assign=True)
             tokenizer = AutoTokenizer.from_pretrained('yiyanghkust/finbert-tone')
             analyser = pipeline('sentiment-analysis', model=model, tokenizer=tokenizer)
-            st.session_state.headlines = [h for h in st.session_state.headlines if isinstance(h, str) and h.strip() != ""]
+            df = st.session_state.headlines
+            df = df[df['headline'].apply(lambda x: isinstance(x, str) and x.strip() != "")]
+            st.session_state.headlines = df
             sentiment_results = get_sentiment(st.session_state.headlines, analyser)
             st.session_state.sentiment_results = sentiment_results
 
