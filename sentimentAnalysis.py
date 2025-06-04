d672c43f7228da413ee72421273eeccb27044921
Mukul Anissh G <31366239+mukul-anissh@users.noreply.github.com>
2025-06-04 15:44:12 +0530

    Update sentimentAnalysis.py

--- sentimentAnalysis.py
+++ sentimentAnalysis.py
@@ -3,20 +3,22 @@ def get_sentiment(headlines, analyser):
     mean_scores = []
     mean_labels_words = []
     for random, row in headlines.iterrows():
-        sector_headlines = row['headlines']
-
-        results = analyser(sector_headlines)
-        score = [res['score'] for res in results]
-        label = [labels[res['label'].lower()] for res in results]
-        mean_score = sum(score) / len(score)
-        mean_label = sum(label) / len(label)
-        if mean_label > 0:
-            mean_labels_words.append('positive')
-        elif mean_label < 0:
-            mean_labels_words.append('negative')
-        else:
-            mean_labels_words.append('neutral')
-        
+        try:
+            sector_headlines = row['headlines']
+    
+            results = analyser(sector_headlines)
+            score = [res['score'] for res in results]
+            label = [labels[res['label'].lower()] for res in results]
+            mean_score = sum(score) / len(score)
+            mean_label = sum(label) / len(label)
+            if mean_label > 0:
+                mean_labels_words.append('positive')
+            elif mean_label < 0:
+                mean_labels_words.append('negative')
+            else:
+                mean_labels_words.append('neutral')
+        except:
+            mean_score = 0
         mean_scores.append(mean_score)
     headlines['mean_score'] = mean_scores
     headlines['mean_label'] = mean_labels_words
